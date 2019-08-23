72d1fff3232994b6623a93bd9c478a6b0b8725a8
Unknown <egonzalezburns@gmail.com>
2019-08-23 05:01:01 -0400

    Fixed auth path regex

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -2,7 +2,7 @@ const firebase = require('../services/firebase/firebase');
 
 function validateHeader(req, res, next) {
     //Protect the only the API
-    if (req.url.match(/^\api/)) {
+    if (req.url.match(/^\/api/)) {
         // If the token is successfully validated against Firebase then we can continue
         firebase.validateToken(req.headers.token).then((_decodedToken) => {
             next();
