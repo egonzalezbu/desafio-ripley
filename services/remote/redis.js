c253dbc5d56b150b8a36cf8d40508f8d3be63458
Unknown <egonzalezburns@gmail.com>
2019-08-23 03:43:12 -0400

    added redis password

--- services/remote/redis.js
+++ services/remote/redis.js
@@ -2,7 +2,8 @@ const redis = require("redis");
 
 const OPTIONS = {
     host: process.env.REDIS_HOST || "127.0.0.1",
-    port: process.env.REDIS_PORT || 6379
+    port: process.env.REDIS_PORT || 6379,
+    password: process.env.REDIS_PASSWORD || null
 };
 
 // Redis EXPIRE Time in Seconds
